4754c0d3c05703d90f8722b87ad6df3de0b0a2df
Marvin Drude <marvin@gidd.io>
2024-03-22 16:43:07 +0100

    Added handshakers default

---
+++ MNet/Tcp/Handshakers/TcpClientHandshaker.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace MNet.Tcp.Handshakers;
+
+public sealed class TcpClientHandshaker : ITcpClientHandshaker {
+
+    public bool Handshake(TcpClient client, ref ReadOnlySequence<byte> buffer, out SequencePosition position) {
+
+        position = buffer.Start; // this method should never be called in default handshaker as start handshaking already true
+        return true;
+
+    }
+
+    public bool StartHandshake(TcpClient client) {
+
+        return true; // just be handshaked upfront
+
+    }
+
+}
---
+++ MNet/Tcp/Handshakers/TcpServerHandshaker.cs
@@ -0,0 +1,19 @@
+﻿
+namespace MNet.Tcp.Handshakers;
+
+public sealed class TcpServerHandshaker : ITcpServerHandshaker {
+
+    public bool Handshake(TcpServerConnection connection, ref ReadOnlySequence<byte> buffer, out SequencePosition position) {
+
+        position = buffer.Start;
+        return true; // just make everyone handshaked without messages
+
+    }
+
+    public void StartHandshake(TcpServerConnection connection) {
+
+        // no action required, everyone instantly handshaked by default
+
+    }
+
+}
